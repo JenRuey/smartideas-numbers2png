8f62294174e3258dd111f6e8ae45753915c347ef
jenruey <jeyrueyng@gmail.com>
2024-04-27 14:40:45 +0800

    firebase setup & bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function ServiceRequestIcon({ texts }: Props) {
       return b.length - a.length;
     }
   )[0].length
-  const lenpercharacter = 17.3
+  const lenpercharacter = 17
   const width = lenpercharacter * highest_length
   const height = 26 * texts.length
   return (
@@ -29,7 +29,7 @@ function ServiceRequestIcon({ texts }: Props) {
       <rect fill="white" width={width} height={height}></rect>
       {texts.map((item, index) => {
         let x_number = (width - (item.length * lenpercharacter)) / 2
-        return <text key={index} x={x_number} y={25 * (index + 1) + (index * 1)} fontWeight="bold" fontSize="30" fill="black">
+        return <text key={index} x={x_number + 2} y={25 * (index + 1) + (index * 1)} fontWeight="bold" fontSize="30px" fill="black" fontFamily='Ariel-Black'>
           {item}
         </text>
       })}
@@ -39,10 +39,11 @@ function ServiceRequestIcon({ texts }: Props) {
 
 function App() {
   const [text, setText] = React.useState("123")
+  const [dataurl, setdataurl] = React.useState("")
 
   const onclick = () => {
     const svg = document.getElementById('print-area-svg');
-    if (svg){
+    if (svg) {
       // // first an doctype
       // var svgDocType = document.implementation.createDocumentType('svg', "-//W3C//DTD SVG 1.1//EN", "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd");
       // // then a new SVG Document
@@ -60,28 +61,58 @@ function App() {
       // link.click();
       // document.body.removeChild(link);
       toPng(svg, { cacheBust: false })
-      .then((dataUrl) => {
-        const link = document.createElement('a');
-        link.href = dataUrl;
-        link.download = "my-image-name.png";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+        .then((dataUrl) => {
+          console.log(dataUrl)
+          // const link = document.createElement('a');
+          // link.href = dataUrl;
+          // link.download = "my-image-name.png";
+          // document.body.appendChild(link);
+          // link.click();
+          // document.body.removeChild(link);
+
+          const img = new Image();
+          img.crossOrigin = "anonymous";
+          img.src = dataUrl;
+          img.onload = () => {
+            // create Canvas
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
+            canvas.width = img.width;
+            canvas.height = img.height;
+            if (ctx) {
+              ctx.drawImage(img, 0, 0);
+              let canvas_dataurl = canvas.toDataURL("image/png");
+              // for create tag anchor
+              const a = document.createElement("a");
+              a.download = `image-download`;
+              a.href = canvas_dataurl
+              a.click();
+
+              setdataurl(canvas_dataurl)
+            }
+          };
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+
     }
   }
 
-
+  console.log(dataurl)
   return (
     <div className="App">
-      <button onClick={onclick} style={{ padding: "20px 50px" }}>Convert</button>
+      <hr/>
       <textarea value={text} onChange={(event) => {
         setText(event.target.value);
-      }} style={{ height: text.split("\n").length * 16 }} />
-      <div style={{ border: "1px solid black" }}>
+      }} style={{ fontSize: "30px", height: text.split("\n").length * 35 , textAlign:'center'}} />
+      <hr/>
+      <button onClick={onclick} style={{ padding: "20px 50px" }}>Convert</button>
+      <hr/>
+      {dataurl &&
+        <img src={dataurl} />}
+      <div style={{ border: "1px solid black", opacity: 0 }}>
         <ServiceRequestIcon texts={text.split("\n")} />
       </div>
     </div>
